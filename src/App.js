0856bb8ad7d2889628f60088942325e94f06de62
sjain.itp <sjain.itp@sparkcognition.com>
2023-02-05 20:05:50 +0530

    fix: first commit made files for redux store action reducer

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">redux</div>;
+}
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+
+const store = createStore(() => 100);
+
+export default store;
